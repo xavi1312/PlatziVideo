dc2cb756a9aa08ce710ecdfa1b3e6857848f85f1
Xavi <xavierisachalemany@gmail.com>
2020-06-24 12:17:03 +0200

    Get videos with fake api and hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 
@@ -11,16 +11,34 @@ import Footer from "../components/Footer";
 import "../assets/styles/App.scss";
 
 const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/initalState")
+      .then((res) => res.json())
+      .then((data) => {
+        setVideos(data);
+      })
+      .catch((err) => console.log("Error fetching videos: " + err));
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Search />
-      <Categories title="Mi lista">
+      {videos.length === undefined && videos.mylist.length > 0 && (
+        <Categories title="Mi lista">
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
+          {videos.length === undefined &&
+            videos.trends.map((item) => (
+              <CarouselItem key={item.id} {...item} />
+            ))}
         </Carousel>
       </Categories>
       <Footer />
