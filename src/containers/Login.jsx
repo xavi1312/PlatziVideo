f4631b702b5f57e0a58df3a8b58fcffc85270d67
Xavi <xavierisachalemany@gmail.com>
2020-06-26 14:21:00 +0200

    send login data to store

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 // Utilities
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { loginRequest } from "../actions";
 // Assets
 import "../assets/styles/components/Login.scss";
 import googleIcon from "../assets/static/google-icon.png";
 import twitterIcon from "../assets/static/twitter-icon.png";
 
-const Login = () => {
+const Login = props => {
   const [form, setValues] = useState({
     email: "",
   });
@@ -18,7 +20,8 @@ const Login = () => {
   };
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(form);
+    props.loginRequest(form);
+    props.history.push("/");
   };
 
   return (
@@ -65,4 +68,8 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
