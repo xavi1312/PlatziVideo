55a7cfbfa85ad2d2a8924b71e94d914de3990e34
Xavi <xavierisachalemany@gmail.com>
2020-07-06 12:19:05 +0200

    add styles to not found component

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,8 +1,21 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react';
+// Utilities
+import { Link } from 'react-router-dom';
+// Components
+import Header from '../components/Header';
+// Assets
+import '../assets/styles/components/NotFound.scss';
 
 const NotFound = () => (
   <Fragment>
-    <h1>No encontrado</h1>
+    <Header isLogin />
+    <section className='notFound'>
+      <div className='notFound__container'>
+        <h1>404 No encontrado</h1>
+
+        <Link to='/'> Volver al inicio </Link>
+      </div>
+    </section>
   </Fragment>
 );
 
